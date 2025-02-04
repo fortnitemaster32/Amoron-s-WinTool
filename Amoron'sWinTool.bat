6363871e2d3d13f474a6a8af8749337ff701783a
fortnitemaster32 <140563856+fortnitemaster32@users.noreply.github.com>
2025-02-04 09:57:52 -0400

    Alpha build v 0.02 Amoron'sWinTool

--- Amoron'sWinTool.bat
+++ Amoron'sWinTool.bat
@@ -1,6 +1,5 @@
 @echo off
 
-
 :main
 cls
 color 5
@@ -11,13 +10,14 @@ echo ┌ 0) exit
 echo ├ 1) minimalist apps install
 echo ├ 2) Full app install
 echo ├ 3) update apps
+echo ├ 4) run tweaks(run as admin)
 :a
 set /p cmd= └
-if '%cmd%'=='0' exit
+if '%cmd%'=='0' exit 
 if '%cmd%'=='1' goto :install
 if '%cmd%'=='2' goto :fulinstall
 if '%cmd%'=='3' goto :update
-echo tested text
+if '%cmd%'=='4' goto :tweaks 
 pause
 goto main
 :install
@@ -42,9 +42,23 @@ winget install --id Microsoft.PowerToys
 winget install --id OBSProject.OBSStudio
 winget install --id WireGuard.WireGuard
 winget install --id MedalB.V.Medal
+winget install --id CharlesMilette.TranslucentTB 
+winget install --id Guru3D.Afterburner 
 goto main
 :update
 winget upgrade --all
+goto main
+:tweaks
+reg add "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseSpeed /t REG_DWORD /d 0 /f >null
+reg add "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold1 /t REG_DWORD /d 0 /f >null
+reg add "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold2 /t REG_DWORD /d 0 /f >null
+echo Mouse accel disabled
+sc config DiagTrack start= disabled
+echo disabled DiagTrack
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "BingSearchEnabled" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "CortanaEnabled" /t REG_DWORD /d 0 /f
+echo disable bing search in WinSearch
+timeout /t 3 /nobreak >nul
 goto main
 :banner
 echo.
